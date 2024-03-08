a9cc58c419f1ddd1c1657c9af581cea76f4c1af8
Tony Tuttle <tuttle.tony@gmail.com>
2024-03-08 13:56:27 -0700

    Section 10

---
+++ app/core/tests/test_admin.py
@@ -0,0 +1,31 @@
+from django.test import TestCase, Client
+from django.contrib.auth import get_user_model
+from django.urls import reverse
+
+
+class AdminSiteTests(TestCase):
+    def setUp(self):
+        self.client = Client()
+        self.admin_user = get_user_model().objects.create_superuser(
+            email="admin@example.com", password="testpass123"
+        )
+        self.client.force_login(self.admin_user)
+        self.user = get_user_model().objects.create_user(
+            email="user@example.com", password="testpass123", name="test user"
+        )
+
+    def test_users_list(self):
+        url = reverse("admin:core_user_changelist")
+        result = self.client.get(url)
+        self.assertContains(result, self.user.name)
+        self.assertContains(result, self.user.email)
+
+    def test_edit_user_page(self):
+        url = reverse("admin:core_user_change", args=[self.user.id])
+        result = self.client.get(url)
+        self.assertEqual(result.status_code, 200)
+
+    def test_create_user_page(self):
+        url = reverse("admin:core_user_add")
+        result = self.client.get(url)
+        self.assertEqual(result.status_code, 200)
